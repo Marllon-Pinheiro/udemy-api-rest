1f0ec3c15c5574c963ba5ee271de2d55dbf40b56
Marllon <marllon_1998@hotmail.com>
2022-09-02 10:57:33 -0300

    initial commit

---
+++ src/main/java/io/github/marllonpinheiro/udemyapirest/UdemyApiRestApplication.java
@@ -0,0 +1,13 @@
+package io.github.marllonpinheiro.udemyapirest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UdemyApiRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UdemyApiRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/marllonpinheiro/udemyapirest/UdemyApiRestApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.marllonpinheiro.udemyapirest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UdemyApiRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
